662b9f986a4bbb30f23fa03c393fe108f0400389
Xavier Navarro <xanasa14@hotmail.com>
2019-04-02 21:01:56 -0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,58 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+ 
+public class main {
+	WebDriver driver;
+	
+	public  void invokeBrowser() {
+		try {
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\chromeDriver\\chromedriver.exe");
+		driver = new ChromeDriver(); 
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+		
+		driver.get("https://www.youtube.com");
+		searching();
+		}catch (Exception e) {e.printStackTrace(); }	
+		
+	}
+	
+	public void searching() throws InterruptedException{
+		try {
+		
+		/* Elements locator techniques 
+		 * id 
+		 * name
+		 * class
+		 * tagname
+		 * cssSelector
+		 * linkText
+		 * partialLinkText
+		 * xpath     
+		 */
+			driver.findElement(By.id("search")).click();
+			driver.findElement(By.id("search")).sendKeys("Paralyzer - Finger Eleven");
+			Thread.sleep(3000);
+			driver.findElement(By.xpath("//*[@id=\"search-icon-legacy\"]")).click();
+
+			Thread.sleep(3000);			
+			driver.findElement(By.linkText("Finger Eleven - Paralyzer lyrics")).click();
+			
+			
+		}catch(InterruptedException e){e.printStackTrace();}
+			
+	}
+	
+	public static void main(String[] args) {
+		
+		main myObj= new main();
+		myObj.invokeBrowser();
+	}
+
+}
